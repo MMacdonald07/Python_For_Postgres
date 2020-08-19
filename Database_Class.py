faf6052e4ea0eac435f8bc1826843f50248665ae
mdmac <matthew.mmacdonald@gmail.com>
2020-08-19 16:54:57 +0100

    Added get_columns and save_table methods

--- Database_Class.py
+++ Database_Class.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+import numpy as np
 
 from psycopg2.extras import execute_values
 from config import user, password, dbname
@@ -18,9 +19,9 @@ class DatabaseConnection:
 
     Methods
     -------
-    create_table(columns, dtypes, id_included=False):
+    create_table(columns, data, id_included=False):
         Creates a new table in the connected database
-    insert_rows(columns, entries):
+    insert_rows(columns, data):
         Inserts rows into specified SQL table
     query(columns, order=None, row_id=None, row_number=None, conditions=None):
         Fetches all rows of table in ascending order by default, or can retrieve a specific ID in table
@@ -38,6 +39,10 @@ class DatabaseConnection:
         Deletes listed rows from table
     drop_table():
         Drops whole table
+    get_columns():
+        Fetches all column names in order from table
+    save_table(path):
+        Saves the SQL table as a CSV file
     close_connection():
         Closes connection to cursor and database
     equal(column_name, values):
@@ -50,6 +55,8 @@ class DatabaseConnection:
         Creates between condition strings in a SQL format
     not_equal(column_name, value):
         Creates not equal to condition strings in a SQL format
+    is_null(column_name):
+        Creates condition strings in a SQL format to find all Null values of a column
     not_null(column_name):
         Creates condition strings in a SQL format to find all non-Null values of a column
     asc(column_name):
@@ -60,6 +67,13 @@ class DatabaseConnection:
 
     def __init__(self, table_name):
         self.table_name = table_name
+        # Takes a python data type and returns the equivalent SQL data type name
+        self.sql_type_conversion = {
+            np.float64: 'real',
+            str: 'varchar(200)',
+            np.int64: 'int',
+            bool: 'bit',
+        }
         try:
             # Establishes connection to PostgreSQL database
             self.connection = psycopg2.connect(
@@ -70,18 +84,23 @@ class DatabaseConnection:
         except:
             print(f'Unable to connect to database {dbname}')
 
-    def create_table(self, columns, dtypes, id_included=False):
+    def create_table(self, columns, data, id_included=False):
         """
         Creates a new SQL table in the connected database
 
         :param columns: Columns present in table_name
         :type columns: list
-        :param dtypes: Data types of each column in table_name
-        :type dtypes: list
+        :param data: Data to be inserted into new table: used to get data types of each column
+        :type data: pd.DataFrame
         :param id_included: Whether or not the table comes with its own ID values for each row
         :type id_included: bool
         :return: None
         """
+        # Scans through DataFrame and determines the equivalent SQL data type of each column
+        try:
+            dtypes = [self.sql_type_conversion[(type(entry))] for entry in data.dropna().iloc[0]]
+        except IndexError:
+            dtypes = [self.sql_type_conversion[(type(entry))] for entry in data.iloc[0]]
         if not id_included:
             # Will produce signature ID column if none included
             create_table_command = f"CREATE TABLE {self.table_name} (id bigserial PRIMARY KEY, "
@@ -93,20 +112,29 @@ class DatabaseConnection:
             for i in range(len(columns) - 1):
                 create_table_command += f"{columns[i]} {dtypes[i]}, "
         create_table_command += f"{columns[-1]} {dtypes[-1]})"
-        self.cursor.execute(create_table_command)
-
+        try:
+            self.cursor.execute(create_table_command)
+        except:
+            print(f'Table {self.table_name} could not be created - it may already exist')
+            return
         print(f'Table "{self.table_name}" successfully created')
 
-    def insert_rows(self, columns, entries):
+    def insert_rows(self, columns, data):
         """
         Inserts new rows into given SQL table
 
         :param columns: Columns present in table_name
         :type columns: list
-        :param entries: List of all values to be inserted into table_name
-        :type entries: list
+        :param data: Dataframe including all rows to be inserted into table_name
+        :type data: pd.DataFrame
         :return: None
         """
+        # Populates all null values in str columns to match those of null floats "nan"
+        for i in range(len(columns)):
+            if type(data[columns[i]].iloc[0]) == str:
+                data[columns[i]].fillna('nan', inplace=True)
+        # Stores all values from file in a tuple for database insertion
+        entries = [row[1:] for row in data.itertuples()]
         if len(entries) == 0:
             print('No data to insert')
             return
@@ -117,13 +145,12 @@ class DatabaseConnection:
         execute_values(self.cursor, insert_command, entries)
         print(str(len(entries)) + ' records successfully inserted into database')
 
-    def query(self, columns, conditions=None, order=None, row_number=None):
+    def query(self, conditions=None, order=None, row_number=None):
         """
         Fetches all table rows by default, or can retrieve a specific ID in table, returning in DataFrame form
 
         :param conditions: Conditions to be included in WHERE statement for overall query
-        :param columns: Columns present in table_name
-        :type columns: list
+        :type conditions: list
         :param order: Either ascending or descending order
         :type order: str
         :param row_number: Number of rows to be retrieved
@@ -133,18 +160,9 @@ class DatabaseConnection:
         """
         # If no row_number or conditions given, will return all rows by default
         if conditions is None:
-            if row_number is not None:
-                self.cursor.execute(f"SELECT * FROM {self.table_name} LIMIT {row_number} ")
-            else:
-                self.cursor.execute(f"SELECT * FROM {self.table_name} ")
-            result = self.cursor.fetchall()
-            df = pd.DataFrame(result, columns=columns)
-            return df
+            query_command = f"SELECT * FROM {self.table_name} "
         else:
             query_command = f" SELECT * FROM {self.table_name} WHERE "
-            # If given as a single condition str, this will be converted to a list
-            if type(conditions) == str:
-                conditions = [conditions]
             if len(conditions) > 1:
                 for i in range(len(conditions) - 1):
                     query_command += f"{conditions[i]} AND "
@@ -152,34 +170,42 @@ class DatabaseConnection:
             else:
                 query_command += conditions[0]
         if order is not None:
-            query_command += f" ORDER BY {order}"
+            query_command += f"ORDER BY {order} "
+        if row_number is not None:
+            query_command += f"LIMIT {row_number} "
         self.cursor.execute(query_command)
         result = self.cursor.fetchall()
+        columns = self.get_columns()
         df = pd.DataFrame(result, columns=columns)
         return df
 
-    def update_rows(self, columns, row_entries, conditions=None):
+    def update_rows(self, columns, data, conditions=None):
         """
         Updates specified row with new data from row_entry
 
         :param conditions: Conditions to be included in WHERE statement for overall query
+        :type conditions: list
         :param columns: Columns present in table_name
         :type columns: list
-        :param row_entries: List of values to be inserted to update row_id
-        :type row_entries: list
+        :param data: Dataframe including all rows to be inserted into table_name
+        :type data: pd.DataFrame
         :return: None
         """
+        for i in range(len(columns)):
+            if type(data[columns[i]].iloc[0]) == str:
+                data[columns[i]].fillna('nan', inplace=True)
+        row_entries = [row[1:] for row in data.itertuples()]
         if conditions is None:
             print('No conditions given, update cannot be completed')
+            return
         elif len(row_entries) == 0:
             print('No new rows given, update cannot be completed')
+            return
         else:
             update_command = f"UPDATE {self.table_name} SET "
             for i in range(len(columns) - 1):
                 update_command += f"{columns[i]} = '{row_entries[0][i]}', "
             update_command += f"{columns[-1]} = '{row_entries[0][-1]}' WHERE "
-            if type(conditions) == str:
-                conditions = [conditions]
             if len(conditions) > 1:
                 for i in range(len(conditions) - 1):
                     update_command += f"{conditions[i]} AND "
@@ -187,14 +213,14 @@ class DatabaseConnection:
             else:
                 update_command += conditions[0]
             self.cursor.execute(update_command)
-            print(f'Rows of table "{self.table_name}" have been updated')
+            print(f'Specified rows have been updated')
 
     def rename_table(self, new_table_name):
         """
         Renames whole table
 
         :param new_table_name: New name of table
-
+        :type new_table_name: str
         :return: None
         """
         if new_table_name == self.table_name:
@@ -241,7 +267,6 @@ class DatabaseConnection:
         :type new_column_names: list
         :return: None
         """
-        rename_column_command = f"ALTER TABLE {self.table_name} RENAME COLUMN"
         if type(old_column_names) == str:
             old_column_names = [old_column_names]
         if type(new_column_names) == str:
@@ -252,19 +277,19 @@ class DatabaseConnection:
                 if old_column_names[i] == new_column_names[i]:
                     continue
                 else:
-                    rename_column_command += f"{old_column_names[i]} TO {new_column_names[i]}, "
+                    rename_column_command = f"ALTER TABLE {self.table_name} RENAME COLUMN "
+                    rename_column_command += f"{old_column_names[i]} TO {new_column_names[i]}"
+                    self.cursor.execute(rename_column_command)
             if old_column_names[-1] != new_column_names[-1]:
-                rename_column_command += f"{old_column_names[-1]} TO {new_column_names[-1]})"
+                rename_column_command = f"ALTER TABLE {self.table_name} RENAME COLUMN "
+                rename_column_command += f"{old_column_names[-1]} TO {new_column_names[-1]}"
+                self.cursor.execute(rename_column_command)
         else:
             if old_column_names[0] != new_column_names[0]:
+                rename_column_command = f"ALTER TABLE {self.table_name} RENAME COLUMN "
                 rename_column_command += f"{old_column_names[0]} TO {new_column_names[0]}"
+                self.cursor.execute(rename_column_command)
 
-        if rename_column_command == f"ALTER TABLE {self.table_name} RENAME COLUMN":
-            # if all names were equivalent
-            print('All names the same, no changes made')
-            return
-
-        self.cursor.execute(rename_column_command)
         print('Columns successfully renamed')
 
     def drop_columns(self, column_names):
@@ -275,7 +300,7 @@ class DatabaseConnection:
         :type column_names: list
         :return: None
         """
-        drop_column_command = f'ALTER TABLE {self.table_name} DROP COLUMN'
+        drop_column_command = f'ALTER TABLE {self.table_name} DROP COLUMN '
         if type(column_names) == str:
             column_names = [column_names]
         if len(column_names) > 1:
@@ -292,6 +317,7 @@ class DatabaseConnection:
         Deletes the list of rows from table_name which satisfy the given conditions
 
         :param conditions: Conditions to be included in WHERE statement for overall query
+        :type conditions: list
         :return: None
         """
         delete_row_command = f"DELETE FROM {self.table_name} "
@@ -300,8 +326,6 @@ class DatabaseConnection:
             self.cursor.execute(delete_row_command)
         else:
             delete_row_command += "WHERE "
-            if type(conditions) == str:
-                conditions = [conditions]
             if len(conditions) > 1:
                 for i in range(len(conditions) - 1):
                     delete_row_command += f"{conditions[i]} AND "
@@ -321,6 +345,36 @@ class DatabaseConnection:
         self.cursor.execute(drop_table_command)
         print(f'Table "{self.table_name}" successfully dropped')
 
+    def get_columns(self):
+        """
+        Uses the information_schema to obtain all columns in a Postgres table
+
+        :return: List of all columns present in SQL table
+        :rtype: list
+        """
+        get_columns_command = "SELECT column_name, ordinal_position FROM information_schema.columns" \
+                              f" WHERE table_name = '{self.table_name}' ORDER BY ordinal_position ASC"
+        self.cursor.execute(get_columns_command)
+        column_positions = self.cursor.fetchall()
+        columns = [column_positions[i][0] for i in range(len(column_positions))]
+        return columns
+
+    def save_table(self, path):
+        """
+        Uses pd.DataFrame.to_csv() to save SQL table as a CSV file
+
+        :param path: The filepath to save the SQL table to
+        :type path: str
+        :return: None
+        """
+        gather_command = f"SELECT * FROM {self.table_name}"
+        self.cursor.execute(gather_command)
+        result = self.cursor.fetchall()
+        columns = self.get_columns()
+        data = pd.DataFrame(result, columns=columns)
+        data.to_csv(path, index=False)
+        print(f'SQL table {self.table_name} successfully saved')
+
     def close_connection(self):
         """
         Closes program's connection to cursor and database
@@ -339,19 +393,16 @@ class DatabaseConnection:
         :param column_name: Name of column inputted
         :type column_name: str
         :param values: Values to be equated to
+        :type values: list
         :return: "column_name" = 'value'
         :rtype: str
         """
-        # if not a list of conditions to check
-        if (type(values) != list) and (type(values) != tuple):
-            condition = f" '{column_name}' = '{values}' "
-        else:
-            condition = '( '
-            for i in range(len(values)):
-                condition += f" '{column_name}' = '{values[i]}' "
-                if i != (len(values) - 1):
-                    condition += "OR "
-            condition += " )"
+        condition = "( "
+        for i in range(len(values)):
+            condition += f" {column_name} = '{values[i]}' "
+            if i != (len(values) - 1):
+                condition += "OR "
+        condition += " )"
         return condition
 
     @staticmethod
@@ -362,11 +413,11 @@ class DatabaseConnection:
         :param column_name: Name of column inputted
         :type column_name: str
         :param value: Value to be compared to
-        :type value: int
+        :type value: str
         :return: "column_name" > 'value'
         :rtype: str
         """
-        condition = f" '{column_name}' > '{value}' "
+        condition = f"{column_name} > {str(value)} "
         return condition
 
     @staticmethod
@@ -377,11 +428,11 @@ class DatabaseConnection:
         :param column_name: Name of column inputted
         :type column_name: str
         :param value: Value to be compared to
-        :type value: int
+        :type value: str
         :return: "column_name" less than 'value'
         :rtype: str
         """
-        condition = f" '{column_name}' < '{value}' "
+        condition = f" {column_name} < '{value}' "
         return condition
 
     @staticmethod
@@ -392,13 +443,13 @@ class DatabaseConnection:
         :param column_name: Name of column inputted
         :type column_name: str
         :param start_value: Lower end of the region.
-        :type start_value: int
+        :type start_value: str
         :param end_value: Higher end of the region.
-        :type end_value: int
+        :type end_value: str
         :return: "column_name" > 'start_value' AND "column_name" less than 'end_value'
         :rtype: str
         """
-        condition = f" '{column_name}' > '{start_value}' AND '{column_name}' < '{end_value}' "
+        condition = f" {column_name} > '{start_value}' AND {column_name} < '{end_value}' "
         return condition
 
     @staticmethod
@@ -412,7 +463,20 @@ class DatabaseConnection:
         :return: "column_name" != 'value'
         :rtype: str
         """
-        condition = f" '{column_name}' != '{value}' "
+        condition = f" {column_name} != '{value}' "
+        return condition
+
+    @staticmethod
+    def is_null(column_name):
+        """
+        Function used to create is null condition strings in a SQL format.
+
+        :param column_name: Name of column inputted
+        :type column_name: str
+        :return: "column_name" is null
+        :rtype: str
+        """
+        condition = f" {column_name} IS NULL "
         return condition
 
     @staticmethod
@@ -425,7 +489,7 @@ class DatabaseConnection:
         :return: "column_name" is not null
         :rtype: str
         """
-        condition = f" '{column_name}' is not null "
+        condition = f" {column_name} IS NOT NULL "
         return condition
 
     @staticmethod
@@ -438,7 +502,7 @@ class DatabaseConnection:
         :return: "column_name ASC"
         :rtype: str
         """
-        return f" '{column_name}' ASC"
+        return f"{column_name} ASC"
 
     @staticmethod
     def desc(column_name):
@@ -450,4 +514,4 @@ class DatabaseConnection:
         :return: "column_name DESC"
         :rtype: str
         """
-        return f" '{column_name}' DESC"
+        return f"{column_name} DESC"
